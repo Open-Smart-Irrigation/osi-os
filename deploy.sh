123dab576ec6e7f502c1b055f385a48f779f62c7
Project-OSI <philippe.hess@opensmartirrigation.org>
2026-08-14 15:32:18 +0200

    fix(deploy): fetch the sdi12 codec (missed fetch_required pair)

--- deploy.sh
+++ deploy.sh
@@ -844,6 +844,10 @@ fetch_required "UC512 codec" \
     "conf/full_raspberrypi_bcm27xx_bcm2712/files/usr/share/node-red/codecs/milesight_uc512_decoder.js" \
     "/srv/node-red/codecs/milesight_uc512_decoder.js"
 
+fetch_required "SDI12 codec" \
+    "conf/full_raspberrypi_bcm27xx_bcm2712/files/usr/share/node-red/codecs/dragino_sdi12_decoder.js" \
+    "/srv/node-red/codecs/dragino_sdi12_decoder.js"
+
 fetch_required "Agroscope uplink transform" \
     "conf/full_raspberrypi_bcm27xx_bcm2712/files/usr/share/node-red/codecs/agroscope_uplink_transform.js" \
     "/srv/node-red/codecs/agroscope_uplink_transform.js"
